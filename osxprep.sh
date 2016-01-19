a49dfb780580f3a812eb9de3a34d6bb92499a503
Thomas <thomasahervey@gmail.com>
2016-01-18 20:52:04 -0800

    more stuff

--- osxprep.sh
+++ osxprep.sh
@@ -14,3 +14,7 @@ sudo softwareupdate -iva # Install all available updates
 echo "------------------------------"
 echo "Installing Xcode Command Line Tools."
 xcode-select --install # Install Xcode command line tools
+
+echo "installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv custom.zshrc.zsh ~/.oh-my-zsh/custom/custom.zshrc.zsh
